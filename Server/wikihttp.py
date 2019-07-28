1570b8209a7cf99061810e7c517a847cf85262f5
Asiri Hewage <foolmashi@gmail.com>
2019-07-29 00:11:52 +0530

    wiki flask impl

--- Server/wikihttp.py
+++ Server/wikihttp.py
@@ -1,3 +1,10 @@
+from __future__ import print_function
+from flask import Flask , jsonify, request
+from flask_cors import CORS
+import os
+import psutil
+import json
+import datetime
 import nltk
 import bs4 as bs 
 import urllib
@@ -63,62 +70,24 @@ def sum(content):
     return summary
 
 ##server --------------------------------------------------------------
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import socketserver
-import json
-import cgi
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+pid = os.getpid()
+py = psutil.Process(pid)
 
-class Server(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        
-    def do_HEAD(self):
-        self._set_headers()
-        
-    # GET sends back a Hello world message
-    def do_GET(self):
-        self._set_headers()
-        self.wfile.write(json.dumps({'hello': 'world', 'received': 'ok'}))
-        
-    # POST echoes the message adding a JSON field
-    def do_POST(self):
-        ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
-        
-        # refuse to receive non-json content
-        if ctype != 'application/json':
-            self.send_response(400)
-            self.end_headers()
-            return
-        try:
-            # read the message and convert it into a python dictionary
-            length = int(self.headers.get('content-length'))
-            message = json.loads(self.rfile.read(length))
-            print(message)
-            # add a property to the object, just to mess with data
-            message['sum'] = sum(message['link'])
-            
-            # send the message back
-            self._set_headers()
-            self.wfile.write(json.dumps(message).encode('utf-8'))
-
-        except:
-            self.send_response(404)
-            self.end_headers()
-            return
-        
-def run(server_class=HTTPServer, handler_class=Server, port=8008):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    
-    print('Starting httpd on port %d...' % port)
-    httpd.serve_forever()
-    
-if __name__ == "__main__":
-    from sys import argv
-    
-    if len(argv) == 2:
-        run(port=int(argv[1]))
+@app.route('/api/v2/wiki', methods=['POST'])
+def get_tasks():
+    content = request.json
+    url = content['url']
+    if url!='':
+        sums = sum(url)
+        data = jsonify(
+            link = url,
+            summary = sums
+            )
     else:
-        run()
+        data  = jsonify()
+    return data
+
+if __name__ == '__main__':
+    app.run(debug=True)
